00db3aeb6a094f6909806ba0818673060a6e70aa
Christopher Evans <silversonic21beast@gmail.com>
2025-09-28 16:36:40 -0500

    Lose condition

--- Assets/Scripts/Scene transitions/EndGame.cs
+++ Assets/Scripts/Scene transitions/EndGame.cs
@@ -1,16 +1,35 @@
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [Header("Settings")]
+    public int maxHits = 3; // How many hits before ending the game
+    public string endSceneName = "LoseScene"; // Name of your end screen scene
+
+    private int hitCount = 0;
+
+    void OnTriggerEnter2D(Collider2D  collision)
     {
+        if (collision.gameObject.CompareTag("Food")) // Customize this tag
+        {
+            hitCount++;
+
+            Debug.Log("Hit #" + hitCount);
+
+                if (hitCount >= maxHits)
+                {
+                    Lose();
+                }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Lose()
     {
-        
+        Debug.Log("Game Over! Loading end screen...");
+        SceneManager.LoadScene(endSceneName);
     }
 }
